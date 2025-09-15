47657e0e90b6a97370915c1c2b4327a34f0bd1eb
YannisLing <3270614976@qq.com>
2025-09-15 18:15:34 +0800

    05cp

--- 05_joinStrings(cp)/joinStrings.js
+++ 05_joinStrings(cp)/joinStrings.js
@@ -9,9 +9,15 @@
 
 // Add your code right below, good luck!
 
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
 
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
-
+const age =thisYear - birthYear;
+const fullName=firstName+" "+lastName;
 
 // Do not change this
 module.exports = {
