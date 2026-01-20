3aca058991eeee37afcd00ed1f22339ecda5ea3c
YannisLing <3270614976@qq.com>
2026-01-20 15:33:18 +0800

    完成10

---
+++ 10_leapYears(cp)/leapYears.js
@@ -0,0 +1,13 @@
+const leapYears = function(year) {
+  if(!Number.isInteger(year)){return "ERROR"}
+  if(year%100===0&&year%400!==0){
+    return false;
+  }else if(year%4===0){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+// Do not edit below this line
+module.exports = leapYears;
