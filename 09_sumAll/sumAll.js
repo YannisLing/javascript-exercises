8cf94a1cd17444004703377bdb2fcd4da46d729a
YannisLing <3270614976@qq.com>
2026-01-20 15:20:01 +0800

    完成09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  if(a<0||b<0||!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR"
+  if(a===b) return a;
+  return (a+b)*(Math.abs(a-b)+1)/2//上底加下底乘以高除以2（等差数列求和） great!!
 };
 
 // Do not edit below this line
