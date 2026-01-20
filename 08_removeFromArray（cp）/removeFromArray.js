f7d2c822a1ac22039e0f06df19f7b22a33f0c679
YannisLing <3270614976@qq.com>
2026-01-20 14:49:35 +0800

    完成08

--- 08_removeFromArray（cp）/removeFromArray.js
+++ 08_removeFromArray（cp）/removeFromArray.js
@@ -1,10 +1,22 @@
 const removeFromArray = function(a,...item) {
+  //遍历item的同时筛选并更新a的复制数组
   let b=a;
   for(let i = 0;i<item.length;i++){
     b=b.filter(m=>m!==item[i]);
   }
   return b;
 };
+/*better solution:
+const removeFromArray = function(a,...item) {
+  //遍历a的元素搬运到一个新的空数组，满足不再item中的才搬运
+  let b =[]
+  a.forEach(itemIna=>{
+    if(!item.includes(itemIna)) b.push(itemIna);
+    })
+  }
+  return b;
 
+
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
