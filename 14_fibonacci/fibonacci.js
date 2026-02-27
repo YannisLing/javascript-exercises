9736a73cba4b35a8affd631ca8c81b6e117d5918
YannisLing <3270614976@qq.com>
2026-02-27 23:07:10 +0800

    完成14,递归法写斐波那契

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  const num = Number(n)
+  if(num<0) return "OOPS"
+  if(num===0) return 0;
+  if(num===1||num===2) return 1;
+  return fibonacci(num-1)+fibonacci(num-2)
 
 };
 
